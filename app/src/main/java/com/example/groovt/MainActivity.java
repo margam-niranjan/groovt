ec81856e921ad54f23148ef4cef6d9ac33befb3a
margam-niranjan <niranjanchintuwin2004@gmail.com>
2024-06-17 20:43:28 +0530

    front-end of login and registration:

---
+++ app/src/main/java/com/example/groovt/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.groovt;
+
+import static androidx.core.view.ViewCompat.setOnApplyWindowInsetsListener;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser() == null){
+            Intent intent = new Intent(MainActivity.this,Login.class);
+            startActivity(intent);
+        }
+    }
+}
